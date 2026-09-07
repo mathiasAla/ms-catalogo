83697f412e2b64e8163e92d49ae9df4f855e34e4
mathiasAla <150154710+mathiasAla@users.noreply.github.com>
2026-09-07 17:37:54 -0300

    Update MsCatalogoApplication.java

--- src/main/java/com/educa/mscatalogo/MsCatalogoApplication.java
+++ src/main/java/com/educa/mscatalogo/MsCatalogoApplication.java
@@ -1,1 +1,11 @@
+package com.educa.mscatalogo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCatalogoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsCatalogoApplication.class, args);
+    }
+}
